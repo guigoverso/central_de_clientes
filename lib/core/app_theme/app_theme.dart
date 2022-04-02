a05826854d2f114e9df821e071a0d0f7d61358b1
Guilherme de O. C. Corona <guilhermeoccorona@gmail.com>
2022-04-02 11:06:15 -0300

    Esquema de cores do aplicativo

---
+++ lib/core/app_theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const appTheme = ColorScheme(
+  primary: Color(0xff7AAED9),
+  onPrimary: Colors.white,
+  secondary: Color(0xff4E82A9),
+  onSecondary: Colors.white,
+  background: Color(0xfff2f2f2),
+  onBackground: Color(0xff121212),
+  brightness: Brightness.light,
+  surface: Color(0xffdedede),
+  onSurface: Color(0xff121212),
+  error: Color(0xffff3333),
+  onError: Colors.white,
+);
